36cf2fe9ba9c0b9d51eb26ce427b230b52f84204
Marydvore <marydvore07@gmail.com>
2026-05-07 20:44:30 +0300

    Added Actions Registration CheckBox

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -3,8 +3,13 @@ package manager;
 import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
@@ -99,5 +104,17 @@ public class HelperUser extends HelperBase {
         //var3
         JavascriptExecutor js = (JavascriptExecutor) wd;
         js.executeScript("document.querySelector('#terms-of-use').click()");
+    }
+
+    public void checkPolicyXY(){
+        WebElement label = wd.findElement(By.cssSelector("label[for ='terms-of-use']"));
+        Rectangle rectangle = label.getRect();
+        int w = rectangle.getWidth();
+        int xOffset=-w/2;
+        Actions actions = new Actions(wd);
+        actions.moveToElement(label,xOffset, 0).click().release().perform();
+
+        //Dimension size = wd.manage().window().getSize(); параметры экрана
+
     }
 }
